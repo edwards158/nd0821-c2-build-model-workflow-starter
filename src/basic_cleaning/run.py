33929492039beabae7e63984aa8c0737f39e3edf
edwards158 <rjedwards158@gmail.com>
2021-09-22 12:39:00 +0100

    update to basic cleaning

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -19,9 +19,6 @@ def go(args):
 
     # Download input artifact. This will also log that this script is using this
     # particular version of the artifact
-    ######################
-    # YOUR CODE HERE     #
-    ######################
 
     logger.info("Downloading artifact")
     artifact_local_path = run.use_artifact(args.input_artifact).file()
@@ -35,6 +32,10 @@ def go(args):
     df = df[idx].copy()
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
+
+    idx = df['longitude'].between(-74.25,
+                                  - 73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
 
     logger.info("Write cleaned data")
     df.to_csv(args.output_artifact, index=False)
